0d4d354f8530323cd44a55b4695b90d108e006f7
Mateus Alfredo Batista <mateusalfredobatista@gmail.com>
2018-06-07 10:59:05 -0300

    Added discovery client service

--- gateway-service/src/main/java/gateway/GatewayService.java
+++ gateway-service/src/main/java/gateway/GatewayService.java
@@ -3,13 +3,11 @@ package gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
-public class ZuulConfig {
+public class GatewayService {
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulConfig.class, args);
+		SpringApplication.run(GatewayService.class, args);
 	}
 }
